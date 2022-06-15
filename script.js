7757acec16837ae4ea094de3fbabc0efcf92ceb6
Jeffrey Tuason <jepoyduque06@gmail.com>
2022-06-15 14:48:52 +0800

    adding tablet style and reset function

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
+// CODED BY: JEFFREY TUASON
+
 const billInput = document.querySelector('.bill__input');
 const peopleLength = document.querySelector('.people__length');
 const tipCustom = document.querySelector('.tip__custom');
 const tipButtons = document.querySelectorAll('.tip-button');
 
-billInput.value = '';
-tipCustom.value = '';
-peopleLength.value = '';
-
 let tipPercentage = 0;
 
 billInput.addEventListener('input', () => calculateAmount(tipPercentage));
-billInput.addEventListener('focusin', e => e.currentTarget.parentElement.classList.add('active'));
-billInput.addEventListener('focusout', e => e.currentTarget.parentElement.classList.remove('active'));
+billInput.addEventListener('focusin', () => billInput.parentElement.classList.add('active'));
+billInput.addEventListener('focusout', () => billInput.parentElement.classList.remove('active'));
 
 tipCustom.addEventListener('input', () => {
     tipButtons.forEach(btn => btn.classList.remove('active-tip'));
     tipPercentage = tipCustom.value;
     calculateAmount(tipPercentage);
 });
-tipCustom.addEventListener('focusin', e => e.currentTarget.classList.add('active'));
-tipCustom.addEventListener('focusout', e => e.currentTarget.classList.remove('active'));
+tipCustom.addEventListener('focusin', () => tipCustom.classList.add('active'));
+tipCustom.addEventListener('focusout', () => tipCustom.classList.remove('active'));
 
 peopleLength.addEventListener('input', () => calculateAmount(tipPercentage));
+peopleLength.addEventListener('focusin', () => peopleLength.parentElement.classList.add('active'));
+peopleLength.addEventListener('focusout', () => peopleLength.parentElement.classList.remove('active'));
 
 tipButtons.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -45,8 +45,10 @@ const calculateAmount = (tipPercentage) => {
         peopleLength.parentElement.parentElement.classList.add('error-people');
         totalValueDisplay.innerHTML = '$0';
         tipAmountValueDisplay.innerHTML = `$0`;
-    }else if(billValue > 0 && tipValue > 0) {
+    } else {
         peopleLength.parentElement.parentElement.classList.remove('error-people');
+    };
+    if(peopleValue > 0 && billValue > 0 && tipValue > 0) {
 
         let totalAmount = billValue * tipValue / peopleValue;
         let totalValue = billValue * tipValue;
@@ -60,12 +62,34 @@ const calculateAmount = (tipPercentage) => {
 };
 
 const decimalFormatter = (val) => {
+    const numArr = val.toString().split('.');
+
     if(val.toString().includes('.')) {
-        const numArr = val.toString().split('.');
         if(numArr[1] > 99) {
             numArr[1] = numArr[1].slice(0,2);
-            return `${numArr[0]}.${numArr[1]}`;
-        };
+            return `$${parseInt(numArr[0]).toLocaleString("en-US")}.${numArr[1]}`
+        } else {
+            return `$${parseInt(numArr[0]).toLocaleString("en-US")}.${numArr[1]}`;
+        }
     };
-    return `$${val}`;
+    return `$${parseInt(numArr[0]).toLocaleString("en-US")}`;
 };
+
+const resetButton = document.querySelector('.reset-button');
+resetButton.addEventListener('click', () => resetValues());
+
+const resetValues = () => {
+    const tipAmountValueDisplay = document.querySelector('.tip-amount__value');
+    const totalValueDisplay = document.querySelector('.total__value');
+
+    tipAmountValueDisplay.innerHTML = `$0`;
+    totalValueDisplay.innerHTML = '$0';
+
+    billInput.value = '';
+    tipCustom.value = '';
+    peopleLength.value = '';
+
+    tipPercentage = 0;
+    tipButtons.forEach(btn => btn.classList.remove('active-tip'));
+};
+resetValues();
